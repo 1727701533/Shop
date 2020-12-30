d78fcd30951a49dd44c22a9af1e211a93ddb02f6
1727701533 <1727701533@qq.com>
2020-12-30 21:35:25 +0800

    查找商品

--- src/ReadProductExcel.java
+++ src/ReadProductExcel.java
@@ -9,21 +9,17 @@ import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class ReadProductExcel {
-    public static void main(String[] args) throws ClassNotFoundException{
-        InputStream in = Class.forName("Test").getResourceAsStream("/product.xlsx");
-        Product[]products=new ReadProductExcel().readExcel(in);
-        for(Product product:products){
-            System.out.println(product.getName());
-            System.out.println("\t"+product.getPrice());
-            System.out.println("\t"+product.getDesc());
-        }
-    }
-    public Product[] readExcel(InputStream in) {
+    /**
+     * 返回了所有的商品
+     * @param in
+     * @return 包含了所有商品的数组
+     */
+    public Product[] getAllProduct(InputStream in) {
         Product products[] = null;
         try {
             XSSFWorkbook xw = new XSSFWorkbook(in);
             XSSFSheet xs = xw.getSheetAt(0);
-            products = new Product[xs.getLastRowNum()];
+            products=new Product[xs.getLastRowNum()];
             for (int j = 1; j <= xs.getLastRowNum(); j++) {
                 XSSFRow row = xs.getRow(j);
                 Product product = new Product();//每循环一次就把电子表格的一行的数据给对象赋值
@@ -50,6 +46,42 @@ public class ReadProductExcel {
         return products;
     }
 
+    /**
+     * 根据ID查找商品
+     * @param id
+     * @param in
+     * @return 返回id对应的商品
+     */
+    public Product getProductById(String id,InputStream in) {
+        try {
+            XSSFWorkbook xw = new XSSFWorkbook(in);
+            XSSFSheet xs = xw.getSheetAt(0);
+            for (int j = 1; j <= xs.getLastRowNum(); j++) {
+                XSSFRow row = xs.getRow(j);
+                Product product = new Product();//每循环一次就把电子表格的一行的数据给对象赋值
+                for (int k = 0; k <= row.getLastCellNum(); k++) {
+                    XSSFCell cell = row.getCell(k);
+                    if (cell == null)
+                        continue;
+                    if (k == 0) {
+                        product.setId(this.getValue(cell));
+                    } else if (k == 1) {
+                        product.setName(this.getValue(cell));
+                    } else if (k == 2) {
+                        product.setPrice(Float.valueOf(this.getValue(cell)));//把字符串转Float
+                    } else if (k == 3) {
+                        product.setDesc(this.getValue(cell));
+                    }
+                }
+                if(id.equals(product.getId())){
+                    return product;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;//如果找不到就返回空
+    }
     private String getValue(XSSFCell cell) {
         String value;
         CellType type = cell.getCellType();
@@ -66,7 +98,7 @@ public class ReadProductExcel {
                 break;
             case NUMERIC:
                 value = df.format(cell.getNumericCellValue());//double和一个空字符串相连接，最终得到字符串
-                System.out.println("处理后的"+value);
+                System.out.println(""+value);
                 break;
             case FORMULA:
                 value = cell.getCellFormula();
--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) throws ClassNotFoundException {
+        /*
+        Ctrl+Alt+L自动排序快捷键
+         */
         boolean bool=true;
         while(bool){
             System.out.println("请输入用户名:");
@@ -14,12 +17,37 @@ public class Test {
 
             //File file=new File("C:\\Users\\G150T\\IdeaProjects\\Shop\\src\\users.xlsx");
             InputStream in=Class.forName("Test").getResourceAsStream("/users.xlsx");
-            ReadUserExcel readExcel=new ReadUserExcel();
+
+            InputStream inPro=Class.forName("Test").getResourceAsStream("/product.xlsx");
+
+            ReadUserExcel readExcel=new ReadUserExcel();//创建对象
             User users[]=readExcel.readExcel(in);
             for(int i=0;i<users.length;i++){
                 if(username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())){
-                    System.out.println("登陆成功");
                     bool=false;
+                    /*
+                    显示商品的信息
+                     */
+                    ReadProductExcel readProductExcel=new ReadProductExcel();
+                    Product products[] = readProductExcel.getAllProduct(inPro);
+                    for (Product product:products) {
+                        System.out.print(product.getId());
+                        System.out.print("\t" + product.getName());//\t类似tab键空格间隔
+                        System.out.print("\t\t" + product.getPrice());
+                        System.out.println("\t\t" + product.getDesc());
+                    }
+                    /*
+                    遍历数组
+                     */
+                    System.out.println("请输入商品ID，把该商品加入购物车");
+                    String pId=sc.next();
+                    ReadProductExcel readProductExcel1=new ReadProductExcel();
+                    inPro=null;
+                    inPro=Class.forName("Test").getResourceAsStream("/product.xlsx");
+                    readProductExcel1.getProductById(pId,inPro);
+                    if(products!=null){
+                        System.out.println("找到该商品");
+                    }
                     break;
                 }else{
                     System.out.println("登陆失败");
@@ -28,3 +56,6 @@ public class Test {
         }
     }
 }
+
+
+
